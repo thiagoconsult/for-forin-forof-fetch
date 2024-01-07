0d74b355263f19a8761731b914dd737e5697c62c
thiago.consultx <thiago.consultx@gmail.com>
2024-01-07 18:48:33 -0300

    Trycatch

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
 const service = require('./service')
 
 async function main() {
-    const result = await service.getUsers()
-    const users = []
-
-    // FOR ========================================================================
-    console.time('FOR')
-    for(let i=0; i< result.length; i++){
-        users.push(result[i].name)
-    }
-    console.timeEnd('FOR')
-    // FOR ========================================================================
-
-
-    // FOR IN =====================================================================
-    console.time('FOR IN')
-    for(let i in result){
-        users.push(result[i].name)
-    }
-    console.timeEnd('FOR IN')
-    // FOR IN =====================================================================
-
-
-    // FOR OF =====================================================================
-    console.time('FOR OF')
-    for(let user of result){
-        users.push(user.nameclear)
-    }
-    console.timeEnd('FOR OF')
-    // FOR OF =====================================================================
+    try{
+        const result = await service.getUsers()
+        const users = []
     
-
-    console.log('USERS: ', users)
+        // FOR ========================================================================
+        console.time('FOR')
+        for(let i=0; i< result.length; i++){
+            users.push(result[i].name)
+        }
+        console.timeEnd('FOR')
+        // FOR ========================================================================
+    
+    
+        // FOR IN =====================================================================
+        console.time('FOR IN')
+        for(let i in result){
+            users.push(result[i].name)
+        }
+        console.timeEnd('FOR IN')
+        // FOR IN =====================================================================
+    
+    
+        // FOR OF =====================================================================
+        console.time('FOR OF')
+        for(let user of result){
+            users.push(user.nameclear)
+        }
+        console.timeEnd('FOR OF')
+        // FOR OF =====================================================================
+    
+    
+        console.log('USERS: ', users)
+    }catch(error){
+        console.error('internal error: ', error)
+    }
 }
 
 main()
